607a8ae8786bb6c03ee51bc13580db6a69b02b17
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2024-06-24 21:57:29 +0100

    component and state managemnt

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import bg from './img/bg.png'
+import { MainLayout } from './styles/Layouts'
+import Orb from "./components/orb/button/Orb";
+
+
+function App() {
+  return (
+    <AppStyled bg={bg} className="App">
+      <Orb />
+      <MainLayout>
+        <h1>App</h1>
+      </MainLayout>
+    </AppStyled>
+  );
+}
+
+
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
+
+export default App;
