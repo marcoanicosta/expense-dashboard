64d5fdf015ec4f9e9a69bb3f650bbfc6cd960f53
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2024-06-24 21:57:53 +0100

    additional files

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off', // Optional: turn off prop-types if using React with hooks and no prop-types validation
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
